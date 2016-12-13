f353143be1d1b5581fa2aff2c6855abc99fef321
nathanABR <nathan.abraham@outlook.fr>
2016-12-13 13:51:29 +0100

    Récupération des commandes en fonction d'une quantité de produits commandé par calcul d'agrégat

--- Insert client commande ligne.sql
+++ Insert client commande ligne.sql
@@ -1,12 +1,12 @@
-INSERT INTO client (nom, prenom, email) VALUES
-	('ABRAHAM', 'Nathan', 'lol@lol.lol'),
-	('JUNG', 'Jean-François', 'jfjung@pourquoi.qui'),
-	('TANCHETTE', 'Vecteur', 'math.vecteur@cosinus.sup');
+--INSERT INTO client (nom, prenom, email) VALUES
+--	('ABRAHAM', 'Nathan', 'lol@lol.lol'),
+--	('JUNG', 'Jean-François', 'jfjung@pourquoi.qui'),
+--	('TANCHETTE', 'Vecteur', 'math.vecteur@cosinus.sup');
 
 INSERT INTO commande (rue, cp, ville, id_client) VALUES
-	('RUE DE LA GAUDINIERE', '44 300', 'Nantes', 1);
+	('22 RUE DU BIGNON', '44 840', 'Les sorinière', 3);
 
 INSERT INTO ligne_commande VALUES
-	(1, 1, 10),
-	(4, 1, 3),
-	(2, 1, 1);
+	(4, 3, 12),
+	(6, 3, 10),
+	(7, 3, 13);
--- select produit.sql
+++ select produit.sql
@@ -2,7 +2,7 @@ Select p.nom AS 'Nom du produit',
 	c.libelle 'Catégorie du produit', 
 	l.quantiteProduit AS 'Quantité commandée', 
 	co.id as 'Numéro de commande', 
-	concat(co.rue,' ',co.cp,' ', co.ville) , 
+	CONCAT(co.rue,' ',co.cp,' ', co.ville) as 'Adresse de livraison', 
 	CONCAT(cl.nom,' ',cl.prenom,' ',cl.email) as 'Informations Client' 
 FROM categorie c 
 	INNER JOIN produit p ON p.id_categorie = c.id 
@@ -10,3 +10,12 @@ FROM categorie c
 	INNER JOIN commande co ON l.id_commande = co.id 
 	INNER JOIN client cl ON cl.id = co.id_client 
 ORDER BY co.id
+
+SELECT co.id as 'Numéro de commande',
+	CONCAT(co.rue,' ',co.cp,' ', co.ville) as 'Adresse de livraison', 
+	CONCAT(cl.nom,' ',cl.prenom,' ',cl.email) as 'Informations Client'
+FROM commande co
+	INNER JOIN client cl ON cl.id = co.id_client
+	INNER JOIN ligne_commande l ON l.id_commande = co.id
+GROUP BY co.id, CONCAT(co.rue,' ',co.cp,' ', co.ville), CONCAT(cl.nom,' ',cl.prenom,' ',cl.email)
+HAVING SUM(l.quantiteProduit) BETWEEN 10 AND 15
